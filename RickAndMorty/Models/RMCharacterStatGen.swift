73832e2dfa98abeaee1f5d5fcf6933d20ca7f791
Easin Md. Hosen <easin.hosen@bengalmobileqa.com>
2023-12-05 13:05:12 +0600

    feat: api request file

---
+++ RickAndMorty/Models/RMCharacterStatGen.swift
@@ -0,0 +1,8 @@
+//
+//  RMCharacterStatGen.swift
+//  RickAndMorty
+//
+//  Created by Easin Md. Hosen on 4/12/23.
+//
+
+import Foundation
---
+++ RickAndMorty/Models/RMOrigin.swift
@@ -0,0 +1,8 @@
+//
+//  RMOrigin.swift
+//  RickAndMorty
+//
+//  Created by Easin Md. Hosen on 4/12/23.
+//
+
+import Foundation
